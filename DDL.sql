e7fa6591ed063e7be92a659b36627fddb6bcc9ba
grimm <sinogrimm@gmail.com>
2026-04-23 16:19:33 -0700

    added DDL file

---
+++ DDL.sql
@@ -0,0 +1,22 @@
+/**
+ * Names: Hunter Shipman, Rebecca Wang
+ * Assignment: Project Step 2 Draft
+ * Description: Data Definition Queries and Sample Data
+
+ The following queries are our own work (edit if needed).
+*/
+
+
+/* CREATE TABLES */
+
+-- create Ranks table
+
+-- ...
+
+
+
+/* INSERT DATA */
+
+-- insert rank data
+
+-- ...
