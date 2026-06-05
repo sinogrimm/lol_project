922dac3e58b35acd79cab919380a6687f971eab4
hs015 <huntershipman15@gmail.com>
2026-06-05 01:24:27 -0700

    updated home page to reflect site functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,18 @@ function Home() {
                 <h2>Developer Information</h2>
                 <p>Team Members: Hunter Shipman, Rebecca Wang</p>
                 <p>Group: 40</p>
+                <p>Our site has the following functionalities implemented:</p>
+                <ul>
+                    <li>reset database</li>
+                    <li>create player (M:M Create)</li>
+                    <li>update player(M:M Update)</li>
+                    <li>delete player (M:M Delete)</li>
+                    <li>update playerrecord (M:M relationship/FK Update)</li>
+                    <li>delete game (M:M relationship Delete)</li>
+                    <li>ability to see every table and its data</li>
+                </ul>
                 <hr />
+
                 <h2>Index</h2>
                 <ul>
                     <li>Home: Navigation</li>
@@ -20,6 +31,7 @@ function Home() {
                     <li>ViewGame: Navigation - Games - View</li>
                     <li>Teams: Navigation</li>
                     <li>PlayerRecords: Navigation</li>
+                    <li>UpdatePlayerRecords: Navigation - PlayerRecords - Edit</li>
                     <li>Ranks: Navigation</li>
                     
                     
